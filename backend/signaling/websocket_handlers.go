2894c21d771f5aeec9c102a591539146dff8c9c6
nakamuraitsuki <isukin@gmail.com>
2025-02-06 20:50:52 +0900

    feat: 接続確立とWebSocket接続によるメッセージ処理を分離

--- backend/signaling/websocket_handlers.go
+++ backend/signaling/websocket_handlers.go
@@ -34,7 +34,10 @@ func (h *WebSocketHandler) Register(g *echo.Group) {
 func (h *WebSocketHandler) HandleWebSocket(c echo.Context) error{
 	req := c.Request()
 	resp := c.Response().Writer
+	userID := c.Get("userID").(string)
+	//TODO: ユーザー認証機能と、ミドルウェアでID設定
 	
+	//接続を取得
 	conn, err := h.upgrader.Upgrade(resp, req, nil)
 	if err != nil {
 		log.Println("Error upgrading connection:", err)
@@ -45,9 +48,36 @@ func (h *WebSocketHandler) HandleWebSocket(c echo.Context) error{
 	}
 	defer conn.Close()
 
+	err = h.manager.AddClient(conn, userID);
+	if err != nil {
+		// エラーが発生した場合、クライアントにエラーメッセージを返す
+		log.Println("Error adding client:", err)
+
+		// IDがすでに登録されている場合は409 Conflict
+		if err.Error() == "client already exists with the given ID" {
+			return c.JSON(http.StatusConflict, map[string]string{
+				"error": "Client with this ID already exists",
+			})
+		}
+		
+		// その他のエラー
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"error": "Internal server error",
+		})
+	}
+
 	// 接続時の処理
 	log.Println("New WebSocket connection established on port", h.service.Port)
 	
-	h.manager.ResetOfferID()
+	//接続によるメッセージ処理をゴルーチンに投げる
+	go h.HandleMessages(conn)
+	
 	return nil
+}
+
+func (h* WebSocketHandler) HandleMessages(conn *websocket.Conn) {
+	defer func() {
+        h.manager.RemoveClient(conn) // クライアントを削除
+        conn.Close() // 接続を閉じる
+    }()	
 }
