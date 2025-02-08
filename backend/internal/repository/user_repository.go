bfe5632da9047ff4de17351a8d1e7d78e820bdad
nakamuraitsuki <isukin@gmail.com>
2025-02-08 19:31:23 +0900

    feat: user_repository作成

---
+++ backend/internal/repository/user_repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"example.com/webrtc-practice/internal/entity"
+)
+
+type UserRepository interface {
+	CreateUser(user *entity.User) error
+	GetAllUsers() ([]*entity.User, error)
+	GetUserByID(id int) (*entity.User, error)
+	GetUserByEmail(email string) (*entity.User, error)
+	Updateuser(user *entity.User) error
+	DeleteUser(id int) error
+}
