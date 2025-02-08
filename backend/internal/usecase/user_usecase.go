d16ed816025ecac4717991aa34d77f3215350851
nakamuraitsuki <isukin@gmail.com>
2025-02-08 20:42:55 +0900

    feat: usecaseにRegisterを追加

--- backend/internal/usecase/user_usecase.go
+++ backend/internal/usecase/user_usecase.go
@@ -1,11 +1,31 @@
 package usecase
 
 import (
-	"example.com/webrtc-practice/internal/domain/service"
 	"example.com/webrtc-practice/internal/domain/repository"
+	"example.com/webrtc-practice/internal/domain/service"
 )
 
 type UserUsecase struct {
-	repo repository.UserRepository
-	hasyer service.Hasher
+	repo   repository.UserRepository
+	hasher service.Hasher
+}
+
+func NewUserUsecase(repo repository.UserRepository, hasher service.Hasher) *UserUsecase {
+	return &UserUsecase{
+		repo:   repo,
+		hasher: hasher,
+	}
+}
+
+func (u *UserUsecase) RegisterUser(name, email, password string) error {
+	hashedPassword, err := u.hasher.HashPassword(password)
+	if err != nil {
+		return err
+	}
+
+	return u.repo.CreateUser(repository.CreateUserParams{
+		Name:      name,
+		Email:     email,
+		PasswdHash: hashedPassword,
+	})
 }
