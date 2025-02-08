d7f11bad873a2299b7398461accc4c8b64538e6c
nakamuraitsuki <isukin@gmail.com>
2025-02-08 19:48:36 +0900

    feat: Hasherを追加

--- backend/internal/usecase/user_usecase.go
+++ backend/internal/usecase/user_usecase.go
@@ -1,9 +1,11 @@
 package usecase
 
 import (
-	"example.com/webrtc-practice/internal/repository"
+	"example.com/webrtc-practice/internal/domain/service"
+	"example.com/webrtc-practice/internal/domain/repository"
 )
 
 type UserUsecase struct {
 	repo repository.UserRepository
+	hasyer service.Hasher
 }
