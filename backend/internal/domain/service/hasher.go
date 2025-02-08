81a49ded3f7e5a7b9b367cbb9df8cc25f8cbc9af
nakamuraitsuki <isukin@gmail.com>
2025-02-08 20:54:27 +0900

    fix: hasherの返り値修正

--- backend/internal/domain/service/hasher.go
+++ backend/internal/domain/service/hasher.go
@@ -2,5 +2,5 @@ package service
 
 type Hasher interface {
 	HashPassword(password string) (string, error)
-	ComparePassword(hashedPassword, password string) error
+	ComparePassword(hashedPassword, password string) (bool, error)
 }
