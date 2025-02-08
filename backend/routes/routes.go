b84ccbf0d6f3f0f60fcb04ed8c5552ff41ed8fca
nakamuraitsuki <isukin@gmail.com>
2025-02-08 22:37:11 +0900

    revert: WebRTC関連を削除

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,15 +2,10 @@ package routes
 
 import (
 	"example.com/webrtc-practice/config"
-	"example.com/webrtc-practice/signaling"
 	
 	"github.com/labstack/echo/v4"
 )
 
-func SetupRoutes(e *echo.Echo, cfg *config.Config, wsHandler *signaling.WebSocketHandler) {
-
-	//webSocket関係のルーティング
-	wsGroup := e.Group("/ws")
-	wsHandler.Register(wsGroup)
-
+func SetupRoutes(e *echo.Echo, cfg *config.Config) {
+	
 }
--- backend/server/server.go
+++ backend/server/server.go
@@ -1,29 +1,14 @@
 package server
 
 import (
-	"net/http"
-
 	"example.com/webrtc-practice/config"
-	"example.com/webrtc-practice/routes"
-	"example.com/webrtc-practice/signaling"
-	
-	"github.com/gorilla/websocket"
+
 	"github.com/labstack/echo/v4"
 )
 
 func ServerStart() {
 	cfg := config.LoadConfig()
 	e := echo.New()
-
-	//WebSocketの依存関係
-	websocketService := &signaling.WebSocketService{Port: cfg.Port}
-	signalingManager := signaling.NewSignalingManager()
-	upgrader := websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool { return true },
-	}
-	websocketHandler := signaling.NewWebSocketHandler(websocketService, signalingManager, upgrader)
-
-	routes.SetupRoutes(e, cfg, websocketHandler)
 
 	e.Logger.Fatal(e.Start(":" + cfg.Port))
 }
