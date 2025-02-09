7df51d83207e9dbb899cef78a3b30f962973a903
nakamuraitsuki <isukin@gmail.com>
2025-02-09 14:03:27 +0900

    feat: JWTのための秘密鍵設定

--- backend/config/config.go
+++ backend/config/config.go
@@ -4,11 +4,13 @@ import	"os"
 
 type Config struct {
 	Port string
+	SecretKey string
 }
 
 func LoadConfig() *Config {
 	return &Config{
 		Port: getEnv("PORT", "8080"),
+		SecretKey: getEnv("SECRET_KEY", "secret"),
 	}
 }
 
