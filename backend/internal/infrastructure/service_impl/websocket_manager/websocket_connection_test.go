fd22c3cbc5f32727f780a638ff6b5d909aed70f2
nakamuraitsuki <ituki1911n@gmail.com>
2025-04-14 23:26:42 +0900

    feat(service_impl): DTOを導入して、秘匿性を保持したままwebsocket接続による送受信

--- backend/internal/infrastructure/service_impl/websocket_manager/websocket_connection_test.go
+++ backend/internal/infrastructure/service_impl/websocket_manager/websocket_connection_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"example.com/webrtc-practice/internal/domain/entity"
+	"example.com/webrtc-practice/internal/infrastructure/dto"
 	websocketmanager "example.com/webrtc-practice/internal/infrastructure/service_impl/websocket_manager"
 	mock_adapter "example.com/webrtc-practice/mocks/interface/adapter"
 	"github.com/stretchr/testify/assert"
@@ -20,26 +21,22 @@ func TestReadMessage(t *testing.T) {
 	conn := websocketmanager.NewWebsocketConnection(mockConn)
 
 	t.Run("正常にメッセージを読み込める", func(t *testing.T) {
-		testMsg := entity.Message{
-			ID:        "123",
-			Type:      "connection",
-			SDP:       "sdp",
-			Candidate: []string{"candidate", "candidate2"},
-			TargetID:  "456",
-		}
-		testMsgByte, _ := json.Marshal(testMsg)
-		mockConn.EXPECT().ReadMessageFunc().Return(1, testMsgByte, nil).Times(1)
+		testMsg := entity.NewMessage("123", "connection", "sdp", []string{"candidate", "candidate2"}, "456")
+		testMsgDto := dto.WebsocketMessageDTO{}
+		testMsgDto.FromEntity(testMsg)
+		response, _ := json.Marshal(&testMsgDto)
+		mockConn.EXPECT().ReadMessageFunc().Return(1, response, nil).Times(1)
 
 		// テスト実行
 		msgType, msg, err := conn.ReadMessage()
 
 		assert.NoError(t, err)
 		assert.Equal(t, 1, msgType)
-		assert.Equal(t, testMsg.ID, msg.ID)
-		assert.Equal(t, testMsg.Type, msg.Type)
-		assert.Equal(t, testMsg.SDP, msg.SDP)
-		assert.Equal(t, testMsg.Candidate, msg.Candidate)
-		assert.Equal(t, testMsg.TargetID, msg.TargetID)
+		assert.Equal(t, testMsg.GetID(), msg.GetID())
+		assert.Equal(t, testMsg.GetType(), msg.GetType())
+		assert.Equal(t, testMsg.GetSDP(), msg.GetSDP())
+		assert.Equal(t, testMsg.GetCandidate(), msg.GetCandidate())
+		assert.Equal(t, testMsg.GetTargetID(), msg.GetTargetID())
 	})
 
 	t.Run("ReadMessageFuncがエラーを返す場合", func(t *testing.T) {
@@ -66,37 +63,31 @@ func TestWriteMessage(t *testing.T) {
 	conn := websocketmanager.NewWebsocketConnection(mockConn)
 
 	t.Run("正常にメッセージを書き込める", func(t *testing.T) {
-		testMsg := entity.Message{
-			ID:        "123",
-			Type:      "connection",
-			SDP:       "sdp",
-			Candidate: []string{"candidate", "candidate2"},
-			TargetID:  "456",
-		}
+		testMsg := entity.NewMessage("123", "connection", "sdp", []string{"candidate", "candidate2"}, "456")
 
 		mockConn.EXPECT().WriteMessageFunc(gomock.Any(), gomock.Any()).Do(func(messageType int, data []byte) {
-			msg := entity.Message{}
+			msg := dto.WebsocketMessageDTO{}
 			err := json.Unmarshal(data, &msg)
 
 			assert.NoError(t, err)
-			assert.Equal(t, testMsg.ID, msg.ID)
-			assert.Equal(t, testMsg.Type, msg.Type)
-			assert.Equal(t, testMsg.SDP, msg.SDP)
-			assert.Equal(t, testMsg.Candidate, msg.Candidate)
-			assert.Equal(t, testMsg.TargetID, msg.TargetID)
+			assert.Equal(t, testMsg.GetID(), msg.ID)
+			assert.Equal(t, testMsg.GetType(), msg.Type)
+			assert.Equal(t, testMsg.GetSDP(), msg.SDP)
+			assert.Equal(t, testMsg.GetCandidate(), msg.Candidate)
+			assert.Equal(t, testMsg.GetTargetID(), msg.TargetID)
 		}).Return(nil).Times(1)
 
-		err := conn.WriteMessage(testMsg)
+		err := conn.WriteMessage(*testMsg)
 
 		assert.NoError(t, err)
 	})
 
 	t.Run("WriteMessageFuncがエラーを返す場合", func(t *testing.T) {
-		testMsg := entity.Message{}
+		testMsg := entity.NewMessage("", "", "", nil, "")
 
 		mockConn.EXPECT().WriteMessageFunc(gomock.Any(), gomock.Any()).Return(errors.New("write error")).Times(1)
 
-		err := conn.WriteMessage(testMsg)
+		err := conn.WriteMessage(*testMsg)
 		assert.Error(t, err)
 	})
 }
